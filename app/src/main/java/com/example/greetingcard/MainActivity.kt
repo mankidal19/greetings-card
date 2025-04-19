7f4547a55d3dab5cb69cc84006318b254fd075ed
mankidal19-mac <man.kidal19@gmail.com>
2025-04-19 09:08:30 +0800

    make Greeting to fillMaxWidth

--- app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -35,10 +36,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Magenta) {
+    Surface(modifier = Modifier.padding(horizontal = 16.dp), color = Color.Magenta) {
         Text(
             text = "Hello my name is $name!",
-            modifier = modifier.padding(24.dp)
+            modifier = modifier
+                        .padding(24.dp)
+                        .fillMaxWidth()
         )
     }
 }
