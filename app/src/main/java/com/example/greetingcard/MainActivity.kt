c025adee26f97836f70fb18e560892c50e469c6d
mankidal19-mac <man.kidal19@gmail.com>
2025-04-19 09:33:31 +0800

    add image and some spacers

--- app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -4,16 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.greetingcard.model.Message
@@ -38,13 +49,23 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MessageCard(message: Message, modifier: Modifier = Modifier) {
-    Column {
-        Text(
-            text = message.author
+    Row {
+        Image(
+            painter = painterResource(R.drawable.mimi),
+            contentDescription = "Mimi",
+            contentScale = ContentScale.Crop,
+            modifier = modifier.size(48.dp).clip(CircleShape)
         )
-        Text(
-            text = message.body
-        )
+        Spacer(modifier = modifier.width(8.dp))
+        Column {
+            Text(
+                text = message.author
+            )
+            Spacer(modifier = modifier.height(4.dp))
+            Text(
+                text = message.body
+            )
+        }
     }
 }
 
